50214ee16b74ecdc0cf4ce1e91baa47357f18283
Irving <Irving@DESKTOP-IRVING>
2017-03-22 00:44:01 -0600

    Implement validation

--- FullStack/FullStack/Controllers/GigsController.cs
+++ FullStack/FullStack/Controllers/GigsController.cs
@@ -31,11 +31,15 @@ namespace FullStack.Controllers
         [HttpPost]
         public ActionResult Create(GigFormViewModel viewModel)
         {
-
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("Create", viewModel);
+            }
             var gig = new Gig
             {
                 ArtistId = User.Identity.GetUserId(),
-                DateTime = viewModel.DateTime ,
+                DateTime = viewModel.GetDateTime(),
                 GenreId = viewModel.Genre,
                 Venue = viewModel.Venue
             };
